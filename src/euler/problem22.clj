466dfb267a0fef8b3d35eeff76c6e67763f42252
Stefc <stefan.boether@gmail.com>
2013-06-05 23:20:54 +0200

    Problem 22 gelöst

---
+++ src/euler/problem22.clj
@@ -0,0 +1,16 @@
+(ns euler.problem22)
+
+(use '[clojure.string :only (split)])
+
+(defn to-sum [s] 
+	( apply + (map #(- (int %) 64) (str s))))
+
+(defn values [s] 
+	(map #(clojure.string/replace % #"\"(.*)\"" "$1") 
+		(clojure.string/split s #",")))
+
+(apply + 
+	(map #(* %1 %2) 
+		(map to-sum 
+			(sort 
+				(vec (values (slurp "http://projecteuler.net/project/names.txt"))))) (iterate inc 1)))
